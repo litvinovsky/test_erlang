31986c472e36401ff70e241a3e2b2760671a5973
Alexander Litvinovsky <alexander.litvinovsky@gmail.com>
2011-08-11 20:55:27 +0300

    balance control for accounts

--- src/accounts.erl
+++ src/accounts.erl
@@ -2,10 +2,10 @@
 -include("../include/records.hrl").
 -compile(export_all).
 
-create(Number) ->
+create(Number, Amount, ReserveAmount) ->
   case check(Number) of
     {account_does_not_exist} ->
-      Obj = new_record(Number, 0),
+      Obj = new_record(Number, Amount, ReserveAmount),
       F = fun() -> mnesia:write(Obj) end,
       mnesia:transaction(F);
     Result -> Result
@@ -19,8 +19,16 @@ all() ->
 
 apply_transaction(A = #account{}, T = #transaction{}) ->
   case T#transaction.confirm of
-    true -> accounts:deposit(A, T#transaction.amount);
-    false -> accounts:reserve(A, T#transaction.amount)
+    true ->
+      case accounts:deposit(A, T#transaction.amount) of
+        {ok, Record} -> Record;
+        Other -> Other
+      end;
+    false ->
+      case accounts:reserve(A, T#transaction.amount) of
+        {ok, Record} -> Record;
+        Other -> Other
+      end
   end.
 
 check(Number) ->
@@ -34,13 +42,45 @@ check(Number) ->
 has_amount(A = #account{}, Amount) when A#account.amount >= abs(Amount) -> true;
 has_amount(_A = #account{}, _Amount) -> false.
 
-new_record(Number, Amount) ->
-  #account{id = erlang:make_ref(), number = Number, amount = Amount}.
+has_reserve_amount(A = #account{}, Amount) when A#account.reserve_amount >= abs(Amount) -> true;
+has_reserve_amount(_A = #account{}, _Amount) -> false.
 
-deposit(Record, Amount) ->
-  OldAmount = Record#account.amount,
-  Record#account{amount=OldAmount + Amount}.
+new_record(Number, Amount, ReserveAmount) ->
+  #account{id = erlang:make_ref(), number = Number, amount = Amount, reserve_amount = ReserveAmount}.
 
-reserve(Record, Amount) ->
-  OldReserveAmount = Record#account.reserve_amount,
-  Record#account{reserve_amount=OldReserveAmount + Amount}.
+deposit(A = #account{}, Amount) when Amount >= 0 ->
+  OldAmount = A#account.amount,
+  NewAmount = OldAmount + Amount,
+  Record = A#account{amount=NewAmount},
+  {ok, Record};
+
+deposit(A = #account{}, Amount) ->
+  case has_amount(A, Amount) of
+    true ->
+      OldAmount = A#account.amount,
+      NewAmount = OldAmount + Amount,
+      Record = A#account{amount=NewAmount},
+      {ok, Record};
+    false -> {error, insufficient_funds}
+  end.
+
+reserve(A = #account{}, Amount) when Amount >= 0 ->
+  case deposit(A, -Amount) of
+    {ok, Record} ->
+      OldReserveAmount = Record#account.reserve_amount,
+      NewReserveAmount = OldReserveAmount + Amount,
+      NewRecord = Record#account{reserve_amount = NewReserveAmount},
+      {ok, NewRecord};
+    Other -> Other
+  end;
+
+reserve(A = #account{}, Amount) ->
+  case has_reserve_amount(A, Amount) of
+    true ->
+      {ok, Record} = deposit(A, abs(Amount)),
+      OldReserveAmount = Record#account.reserve_amount,
+      NewReserveAmount = OldReserveAmount + Amount,
+      NewRecord = Record#account{reserve_amount = NewReserveAmount},
+      {ok, NewRecord};
+    false -> {error, insufficient_reserve_funds}
+  end.
